36c6c939e53bcfdba2030d80dc38a01f6b61f770
Michael Glatzhofer <Michael.Glatzhofer@student.tugraz.at>
2018-01-03 07:05:46 +0100

    ping missing

--- src/components/meta/unitdisk-meta/unitdisk-meta.ts
+++ src/components/meta/unitdisk-meta/unitdisk-meta.ts
@@ -59,9 +59,16 @@ interface UnitdiskMeta_UI {
 
 const colors         = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
 const typeColors     = colors
+
+const maxTime = 25     // 25ms
+const maxSvg = 1000
+const maxD3 = maxTime  
+const maxLoad = 1000   // 1000ms
+
+
 const mag_svg        = .1 // 1000#  ?
 const mag_load       = 10 // 1000ms
-const mag_time       = 4  // 25ms
+const mag_time       = 4  // 25ms 
 
 var updateBar = (view, vec, cvec)=> {
     var c=0
@@ -95,32 +102,54 @@ var row = `
 
 var sliderrow = (id, classes='')=> `
     <div class="label"> </div> 
-    <div class="nodes slider">
+    <div class="sm"><sub>2</sub></div> 
+    <div class="slider">
         <p class="range-field">
             <input type="range" min="2" max="500" value="160" class="slider" id="myRange">
         </p>
-    </div> 
-    <div class="q"></div> 
+    </div>     
     <div class="qmax"></div> 
-    <div class="info"></div>
-    <div class="info2"></div>
-    <div class="bar-bg"></div>`
+    <div class="info-oneRowSpan"></div>
+    <div class="info2-oneRowSpan"></div>`
 
 var work = `
     <div class="label"></div> 
-    <div class="nodes"></div> 
-    <div class="q"></div> 
+    <div class="min"><sub>1</sub></div> 
+    <div class="hist">
+        <div style="height:100%"></div>
+        <div style="height:70%"></div>
+
+        <div style="height:30%"></div>
+        <div style="height:45%"></div>
+        <div style="height:70%"></div>
+        <div style="height:60%"></div>
+
+        <div style="height:80%"></div>
+        <div style="height:65%"></div>
+        <div style="height:30%"></div>
+        <div style="height:20%"></div>
+
+        <div style="height:25%"></div>
+        <div style="height:15%"></div>
+        <div style="height:7%"></div>
+        <div style="height:8%"></div>
+
+        <div style="height:7%"></div>
+        <div style="height:5%"></div>
+        <div style="height:5%"></div>
+        <div style="height:5%"></div>
+    </div> 
     <div class="qmax"></div> 
-    <div class="info-1"></div>
-    <div class="info2-1"></div>`
+    <div class="info-oneRowSpan"></div>
+    <div class="info2-oneRowSpan"></div>`
 
 var htmlinfo = 
     `<div class="render-info">
         ${row}
         ${row}
+        ${row}        
         ${sliderrow('')}
-        ${work}
-        ${work}
+        ${sliderrow('')}
         ${row}        
         ${work}
         ${work}
@@ -131,7 +160,6 @@ var htmlinfo =
 
 function hypertreeMeta_({ parent, ud, className })
 {
-
 }
 
 function UnitdiskMeta_({ parent, ud, className })
@@ -143,19 +171,23 @@ function UnitdiskMeta_({ parent, ud, className })
     var e = 0
     var re = 7
     var rows = {                
-        rendering: new BarRow   (ui, e+=0,  'SVG',                '<sub>#</sub>'),
-        d3:        new BarRow   (ui, e+=re, 'D<sub>3</sub>',      '<sub>ms</sub>'),
-        transform: new BarRow   (ui, e+=re, 'Transf.',            '<sub>ms</sub>'),     
-        cullmaxw:  new TextRow  (ui, e+=re, '<sub>W<sub>max</sub></sub>', '<sub>6k</sub>'),
-        lambda:    new TextRow  (ui, e+=6,  '<sub>λ</sub>',       '<sub>1</sub>'),
-        layout:    new BarRow   (ui, e+=6,  'Layout',             '<sub>ms</sub>'),        
-        degree:    new TextRow  (ui, e+=re, '<sub>Degrees</sub>', '<sub>97</sub>'), 
-        weights:   new TextRow  (ui, e+=6,  '<sub>Weights</sub>', '<sub>34k</sub>'),
-        heights:   new TextRow  (ui, e+=6,  '<sub>Heights</sub>', '<sub>79</sub>'),
-        data:      new BarRow   (ui, e+=6,  'Ajax',               '<sub>s</sub>'),
-        lang:      new BarRow   (ui, e+=re, 'Lang',               '<sub>s</sub>'),
+        rendering: new BarRow   (ui, e+=0,  'SVG',                         '<sub>#</sub>'),
+        d3:        new BarRow   (ui, e+=re, 'D<sub>3</sub>',               '<sub>ms</sub>'),
+        transform: new BarRow   (ui, e+=re, '∀<sub>visible</sub>',         '<sub>ms</sub>'),     
+        cullmaxw:  new SliderRow(ui, e+=re, '<sub>ω<sub>cull</sub></sub>', '<sub>.5k</sub>'),
+        lambda:    new SliderRow(ui, e+=6,  '<sub>λ</sub>',                '<sub>1</sub>'),
+        layout:    new BarRow   (ui, e+=6,  'Select',                      '<sub>ms</sub>'),        
+        degree:    new TextRow  (ui, e+=re, '<sub>δ</sub>',                '<sub>97</sub>'), 
+        weights:   new TextRow  (ui, e+=6,  '<sub>ω</sub>',                '<sub>34k</sub>'),
+        heights:   new TextRow  (ui, e+=6,  '<sub>τ</sub>',                '<sub>79</sub>'),
+        data:      new BarRow   (ui, e+=6,  'Load',                        '<sub>s</sub>'),
+        lang:      new BarRow   (ui, e+=re, 'Lang',                        '<sub>s</sub>'),
     }
     
+    function ping(v) {        
+        //v.classList.add("anim2");
+    }
+
     // zu slider row
     var slider = ui.querySelector('.range-field > input')
     ud.args.hypertree.magic = 1/slider.value
@@ -174,9 +206,14 @@ function UnitdiskMeta_({ parent, ud, className })
                 Δ.push(elemCount)
             }
         var a = Δ.reduce((a,e)=> a+e, 0).toFixed(0)
+                
+        const v = rows.rendering
+        v.q.innerHTML = `${a}` 
         
-        updateBar(rows.rendering.bar, Δ.map(e=> e*mag_svg), typeColors)        
-        rows.rendering.q.innerHTML = `${a}` 
+        updateBar(v.bar, Δ.map(e=> e*mag_svg), typeColors)                
+        ping(v.useIndic)
+        if (parseFloat(a) > maxSvg)
+            ping(v.overuseIndic)        
     }
  
     ui.updateD3Info = ()=> { 
@@ -185,11 +222,16 @@ function UnitdiskMeta_({ parent, ud, className })
         var layerlist = ud.layerStack.d3meta.names
 
         var t = Δ.reduce((a,e)=> a+e).toFixed(0)
+
+        const v = rows.d3
         
-        rows.d3.info.innerHTML = `${cache.unculledNodes.length} nodes`
-        rows.d3.info.title     = Δ.map((e, i)=> `${layerlist[i]}: ${e.toFixed(1)}ms`).join('\n')
-        rows.d3.q.innerHTML    = `${t}`
-        updateBar(rows.d3.bar, Δ.map(e=> e*mag_time), typeColors)
+        v.info.innerHTML = `${cache.unculledNodes.length} nodes`
+        v.info.title     = Δ.map((e, i)=> `${layerlist[i]}: ${e.toFixed(1)}ms`).join('\n')
+        v.q.innerHTML    = `${t}`
+
+        updateBar(v.bar, Δ.map(e=> e*mag_time), typeColors)
+        v.useIndic.style.backgroundColor = true ? '#666' : 'none'
+        v.overuseIndic.style.backgroundColor = (parseFloat(t) > maxTime) ? 'red' : 'transparent'
     }
 
     ui.updateTransformationInfo = ()=> {        
@@ -201,23 +243,31 @@ function UnitdiskMeta_({ parent, ud, className })
         var na = cache.unculledNodes.length
         var hwexits = minWeight.map(n=>n.toFixed(1)).join(' ⟶ ')
         var Δms = Δ.map(n=>n.toFixed(1))
-         
-        updateBar(rows.transform.bar, Δ.map(e=> e*mag_time), ['#2196f3', '#ffc107', '#673ab7', '#4caf50'])        
-        rows.transform.useIndic.style.backgroundColor = true ? '#666' : 'none'
-        rows.transform.overuseIndic.style.backgroundColor = true ? 'red' : 'none'
-        //transformInfo.innerHTML  = `${na} nodes<sub>w > ${'...'}</sub>`
-        rows.transform.info.title      = `Visible node count: ${na}\n`        
-        rows.transform.info.title     += `Min weigth: ${hwexits}\n` 
-        rows.transform.info.title     += `${Δms[0]} culling\n${Δms[1]} lazysearch\n${Δms[2]} voronoi\n${Δms[3]} labels`
-        rows.transform.q.innerHTML     = `${t}`        
+        
+        const v = rows.transform
+
+        v.info.innerHTML  = `sets, voro, labels, ...`
+        v.info.title      = `Visible node count: ${na}\n`        
+        v.info.title     += `Min weigth: ${hwexits}\n` 
+        v.info.title     += `${Δms[0]} culling\n${Δms[1]} lazysearch\n${Δms[2]} voronoi\n${Δms[3]} labels`
+        v.q.innerHTML     = `${t}`        
+
+        updateBar(v.bar, Δ.map(e=> e*mag_time), ['#2196f3', '#ffc107', '#673ab7', '#4caf50'])        
+        v.useIndic.style.backgroundColor = true ? '#666' : 'none'
+        v.overuseIndic.style.backgroundColor = (parseFloat(t) > maxTime) ? 'red' : 'transparent'
     }
 
     ui.updateLayout = ()=> {        
         var Δ = ud.args.hypertree.layoutMeta.Δ
-              
-        updateBar(rows.layout.bar, [Δ].map(e=> e*mag_time), ['#2196f3'])        
-        rows.layout.info.innerHTML     = `32 • 77 • 4k`
-        rows.layout.q.innerHTML     = `${Δ.toFixed()}`        
+        
+        const v = rows.layout
+
+        v.info.innerHTML  = `32 • 77 • 4k`
+        v.q.innerHTML     = `${Δ.toFixed()}`        
+
+        updateBar(v.bar, [Δ].map(e=> e*mag_time), ['#2196f3'])        
+        v.useIndic.style.backgroundColor = true ? '#666' : 'none'
+        v.overuseIndic.style.backgroundColor = (parseFloat(Δ) > maxTime) ? 'red' : 'transparent'
     }
 
     const d3format = d3.format('.3s')
@@ -235,31 +285,40 @@ function UnitdiskMeta_({ parent, ud, className })
         var h = model.height
         var ø = 0; model.each(cn=> ø += (cn.children||[]).length/i)
 
-        updateBar(rows.data.bar, Δ.map(e=>e/mag_load), ['#ff9800', '#2196f3', 'green'])        
-        rows.data.info.innerHTML  = `${d3format(n)} nodes • 3kB` 
-        rows.data.info.title      = `download: ${Δ[0].toFixed(0)}ms\n`
-        rows.data.info.title     += `parse: ${Δ[1].toFixed(0)}ms\n`
-        rows.data.info.title     += `hierarchy and weights: ${Δ[2].toFixed(0)}ms\n`
-        rows.data.info.title     += `${lp} leaves\n`
-        rows.data.info.title     += `↕ max: ${h}\n`
-        rows.data.info.title     += `↕ μ: ?\n`
-        rows.data.info.title     += `↕ ⌀: ?\n`
-        rows.data.info.title     += `○ max: ?\n`
-        rows.data.info.title     += `○ μ: ${ø.toPrecision(2)}\n`
-        rows.data.info.title     += `○ ⌀: ?\n`
-        rows.data.q.innerHTML     = `${(t/1000).toFixed(1)}`        
+        const v = rows.data
+
+        v.info.innerHTML  = `${d3format(n)} nodes • 3kB` 
+        v.info.title      = `download: ${Δ[0].toFixed(0)}ms\n`
+        v.info.title     += `parse: ${Δ[1].toFixed(0)}ms\n`
+        v.info.title     += `hierarchy and weights: ${Δ[2].toFixed(0)}ms\n`
+        v.info.title     += `${lp} leaves\n`
+        v.info.title     += `↕ max: ${h}\n`
+        v.info.title     += `↕ μ: ?\n`
+        v.info.title     += `↕ ⌀: ?\n`
+        v.info.title     += `○ max: ?\n`
+        v.info.title     += `○ μ: ${ø.toPrecision(2)}\n`
+        v.info.title     += `○ ⌀: ?\n`
+        v.q.innerHTML     = `${(t/1000).toFixed(1)}`        
+
+        updateBar(v.bar, Δ.map(e=>e/mag_load), ['#ff9800', '#2196f3', 'green'])        
+        v.useIndic.style.backgroundColor = true ? '#666' : 'none'
+        v.overuseIndic.style.backgroundColor = (parseFloat(t) > 1000) ? 'red' : 'transparent'
     }
     
     ui.updateLang = ()=> {        
         var Δ = ud.args.hypertree.langMeta.Δ 
         var model = ud.args.hypertree.data
+        const Δs = Δ/1000
 
         // do the hole DSIT STUFF!
         const v = rows.lang
 
-        updateBar(v.bar, Δ.map(e=>e/mag_load), ['#ff9800', '#2196f3', 'green'])        
         v.info.innerHTML  = `34k • 7k • 1k • 34`
-        v.q.innerHTML     = `0.3`
+        v.q.innerHTML     = Δs.toFixed(1)
+
+        updateBar(v.bar, Δ.map(e=>e/mag_load), ['#ff9800', '#2196f3', 'green'])        
+        v.useIndic.style.backgroundColor = true ? '#666' : 'none'
+        v.overuseIndic.style.backgroundColor = (Δ > 1000) ? 'red' : 'transparent'
     }
 
     return ui
@@ -292,11 +351,18 @@ class BarRow extends TextRow
     }
 }
 
-class SliderRow extends TextRow
+class SliderRow 
 {
     label; info; q; qMax; w; bar
     constructor(ui, offset, desc, unit) {
-        super(ui, offset, desc, unit)
+        this.label = <HTMLElement>ui.children[offset+0]
+        this.info =  <HTMLElement>ui.children[offset+1]        
+        this.qMax =  <HTMLElement>ui.children[offset+3]
+        this.w =     <HTMLElement>ui.children[offset+4]
+        
+        this.label.innerHTML = desc
+        // this.info.innerHTML = '0'
+        this.qMax.innerHTML = unit
     }
 }
 
