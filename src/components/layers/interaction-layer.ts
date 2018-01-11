59831b405c3d1600dacbe64f61fa29997cb57a81
Michael Glatzhofer <Michael.Glatzhofer@student.tugraz.at>
2018-01-11 21:16:54 +0100

    layout wheel bug

--- src/components/layers/interaction-layer.ts
+++ src/components/layers/interaction-layer.ts
@@ -112,7 +112,7 @@ export class InteractionLayer implements ILayer
                         )
                     }
                 }
-                else*/ {
+                */else {
                     this.onDragByNode(
                         dragStartElement,
                         dragStartPoint,
