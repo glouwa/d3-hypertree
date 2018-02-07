5241fb0972c2505575a90deaebb859593f1f1798
Michael Glatzhofer <Michael.Glatzhofer@student.tugraz.at>
2018-02-07 03:35:40 +0100

    light colors, slider layout

--- src/components/meta/unitdisk-meta/unitdisk-meta.ts
+++ src/components/meta/unitdisk-meta/unitdisk-meta.ts
@@ -69,6 +69,7 @@ var sliderrow = (id, min, max, val, classes='')=> `
             <input type="range" min="${min}" max="${max}" value="${val}" class="slider" id="myRange" step="0.01">
         </p>
     </div>     
+    <div class="q left-aligned"></div> 
     <div class="qmax"></div> 
     <div class="info-oneRowSpan"></div>
     <div class="info2-oneRowSpan"></div>`
@@ -217,8 +218,8 @@ function UnitdiskMeta_({ parent, ud, className })
         d3:        new BarRow   (ui, e+=re, 'D<sub>3</sub>',       '<sub>ms</sub>'),
         transform: new BarRow   (ui, e+=re, '∀<sub>visible</sub>', '<sub>ms</sub>'),     
         cullmaxw:  new SliderRow(ui, e+=re, 'ω<sub>cull</sub>',    '<sub>.5k</sub>', ωsliderInit, v=> `<sub>${v}</sub>`),
-        lambda:    new SliderRow(ui, e+=6,  'λ',                   '<sub>1</sub>', λsliderInit, v=> `<sub>.${v*10}</sub>`),
-        layout:    new BarRow   (ui, e+=6,  'Select',              '<sub>ms</sub>'),        
+        lambda:    new SliderRow(ui, e+=7,  'λ',                   '<sub>1</sub>', λsliderInit, v=> `<sub>.${v*10}</sub>`),
+        layout:    new BarRow   (ui, e+=7,  'Filter',              '<sub>ms</sub>'),        
         degree:    new HistRow  (ui, e+=re, 'δ<sup>+</sup>',       '<sub>97</sub>'), 
         weights:   new HistRow  (ui, e+=7,  'ω',                   '<sub>34k</sub>'),
         heights:   new HistRow  (ui, e+=7,  'τ',                   '<sub>79</sub>'),
