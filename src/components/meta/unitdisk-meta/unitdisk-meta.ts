f0cbd630285cdc8fbbca0c611831d7cd7731e1d9
Michael Glatzhofer <Michael.Glatzhofer@student.tugraz.at>
2018-02-25 17:19:53 +0100

    centernode stroke

--- src/components/meta/unitdisk-meta/unitdisk-meta.ts
+++ src/components/meta/unitdisk-meta/unitdisk-meta.ts
@@ -97,21 +97,6 @@ var histrow = `
     <div class="qmax"></div> 
     <div class="info-oneRowSpan"></div>
     <div class="info2-oneRowSpan"></div>`
-/*
-var htmlinfo = 
-    `<div class="render-info">
-        ${barrow}
-        ${barrow}
-        ${barrow}        
-        ${sliderrow('', 2, 500, 160)}
-        ${sliderrow('', .4, .9, .5)}
-        ${barrow}        
-        ${histrow}
-        ${histrow}
-        ${histrow}
-        ${barrow}
-        ${barrow}
-    </div>`*/
 
 var htmlinfo = 
     `<div class="render-info">
@@ -226,21 +211,7 @@ function UnitdiskMeta_({ parent, ud, className })
     const ωsliderInit = (sliderHtml)=> sliderInit(sliderHtml, sliderBindingω)
 
     var e = 0
-    var re = 7 
-    /*
-    var rows = {                
-        rendering: new BarRow   (ui, e+=0,  'Σ',                   '<sub>ms</sub>'),
-        d3:        new BarRow   (ui, e+=re, 'D<sub>3</sub>',       '<sub>ms</sub>'),
-        transform: new BarRow   (ui, e+=re, '∀<sub>visible</sub>', '<sub>ms</sub>'),     
-        cullmaxw:  new SliderRow(ui, e+=re, 'ω<sub>cull</sub>',    '<sub>.5k</sub>', ωsliderInit, v=> `<sub>${v}</sub>`),
-        lambda:    new SliderRow(ui, e+=7,  'λ',                   '<sub>1</sub>', λsliderInit, v=> `<sub>.${v*10}</sub>`),
-        layout:    new BarRow   (ui, e+=7,  'Filter',              '<sub>ms</sub>'),        
-        degree:    new HistRow  (ui, e+=re, 'δ<sup>+</sup>',       '<sub>97</sub>'), 
-        weights:   new HistRow  (ui, e+=7,  'ω',                   '<sub>34k</sub>'),
-        heights:   new HistRow  (ui, e+=7,  'τ',                   '<sub>79</sub>'),
-        data:      new BarRow   (ui, e+=7,  'Load',                '<sub>s</sub>'),
-        lang:      new BarRow   (ui, e+=re, 'Lang',                '<sub>s</sub>'),
-    }*/
+    var re = 7     
     var rows = {
         lang:      new BarRow   (ui, e+=0,  'Lang',                '<sub>s</sub>'),        
         heights:   new HistRow  (ui, e+=7,  'τ',                   '<sub>79</sub>'),
