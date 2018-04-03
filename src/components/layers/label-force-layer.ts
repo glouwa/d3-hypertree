7969652b980132126d20b32c32aa45560bd8402f
Michael Glatzhofer <Michael.Glatzhofer@student.tugraz.at>
2018-04-03 23:12:34 +0200

    force .002

--- src/components/layers/label-force-layer.ts
+++ src/components/layers/label-force-layer.ts
@@ -74,7 +74,7 @@ export class LabelForceLayer implements ILayer
             .force("charge",  d3.forceManyBody()
                 .strength(-.05))
             .force("collide", d3.forceCollide()
-                .strength(.003)
+                .strength(.002)
                 .radius(.18)) // .18
             .force('gravity', d3f(0,0)                
                 .strength(-.001))
